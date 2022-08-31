0e1750914a532515196869443410735f6b570af0
Maximilian Haye <m.haye@tu-berlin.de>
2022-08-31 17:27:36 +0200

    style: add copyright notices

--- tests/form/elements/element_json_test.php
+++ tests/form/elements/element_json_test.php
@@ -1,9 +1,25 @@
 <?php
+// This file is part of the QuestionPy Moodle plugin - https://questionpy.org
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 namespace qtype_questionpy\form\elements;
 
 class element_json_test extends \advanced_testcase {
     /**
      * @dataProvider deserialize_provider
+     * @covers \qtype_questionpy\form\elements
      */
     public function test_deserialize(string $jsonfile, $expected): void {
         $json = file_get_contents(__DIR__ . "/json/" . $jsonfile);
@@ -16,6 +32,7 @@ class element_json_test extends \advanced_testcase {
 
     /**
      * @dataProvider serialize_provider
+     * @covers \qtype_questionpy\form\elements
      */
     public function test_serialize(string $expectedjsonfile, $value): void {
         $actualjson = json_encode($value);
