d978056549c76f992e165a446673879b0cca8e5d
janbritz <britz@tu-berlin.de>
2022-09-27 11:40:48 +0200

    docs: updated copywrite notice

--- lib.php
+++ lib.php
@@ -1,18 +1,18 @@
 <?php
-// This file is part of QuestionPy - www.questionpy.org
+// This file is part of the QuestionPy Moodle plugin - https://questionpy.org
 //
-// QuestionPy is free software: you can redistribute it and/or modify
+// Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// QuestionPy is distributed in the hope that it will be useful,
+// Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with QuestionPy.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Serve question type files
--- tests/package_test.php
+++ tests/package_test.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of the QuestionPy Moodle plugin - https://questionpy.org
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace qtype_questionpy;
-
 
 use TypeError;
 
