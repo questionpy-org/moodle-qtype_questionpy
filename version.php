5c98d88053e5b54f98536ec825cd2a43eb4b124f
Maximilian Haye <m.haye@tu-berlin.de>
2024-03-14 14:43:15 +0100

    chore: raise required moodle core version to 4.1.0

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_questionpy';
 $plugin->version = 2024022900;
-$plugin->requires = 2022041901;
+$plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.1';
