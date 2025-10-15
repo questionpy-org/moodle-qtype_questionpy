6754957eb8b300234f2b0b4bb358c236180d2817
Jan Britz <britz@tu-berlin.de>
2025-10-15 16:25:13 +0200

    fix: handle context outside of a course

--- classes/question_service.php
+++ classes/question_service.php
@@ -139,7 +139,9 @@ class question_service {
                 $pkgversionhash = $pkgversion->hash;
                 $pkgversionnamespace = $package->namespace;
                 $pkgversionshortname = $package->shortname;
-                last_used_service::add($question->context->get_course_context()->id, $package->id);
+                $coursecontext = $question->context->get_course_context(strict: false);
+                $context = $coursecontext ?: $question->context;
+                last_used_service::add($context->id, $package->id);
             } else {
                 $packageinfo = $this->api->get_package_info($question->qpy_package_hash);
                 $pkgversionhash = $packageinfo->hash;
