146e39d6dcb6e83c534dc9a6c51de2de0f777ef2
Maximilian Haye <m.haye@tu-berlin.de>
2025-10-23 12:16:23 +0200

    test: fix test_upsert_question_should_save_draft_files

--- classes/question_service.php
+++ classes/question_service.php
@@ -217,7 +217,10 @@ class question_service {
 
         // Save the draft area files belonging to the question.
         // file_upload_element and wysiwyg_editor_element add to qpy_options_draftitems.
-        $draftitems = array_filter((array) $question->qpy_options_draftitems ?? [], fn($draftitemid) => is_int($draftitemid));
+        $draftitems = array_filter(
+            isset($question->qpy_options_draftitems) ? (array) $question->qpy_options_draftitems : [],
+            fn($draftitemid) => is_int($draftitemid)
+        );
         global $USER;
         if ($draftitems) {
             $this->ofs->save_draft_area_files($question->context->id, $question->id, $USER->id, $draftitems);
